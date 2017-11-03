d2a8aaba3236b09b2cefa37eaf7afa30b6eb4087
Henning Luick <henningel@gmail.com>
2017-11-03 23:19:50 +0100

    added rotating platforms. (hacked half rotating)

---
+++ ShiverClock/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject target; //target to follow
+    public Vector2 targetOffset;
+    public Color fadeColor;
+    private Texture2D fadeTexture;
+    public float defaultFadeTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        targetOffset = new Vector3(0, 0, 0);
+        fadeTexture = new Texture2D(1, 1, TextureFormat.ARGB32, false);
+        fadeTexture.SetPixel(0, 0, fadeColor);
+        fadeTexture.Apply();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (target != null)
+        {
+            float followSpeed = 1.0f;
+            Vector3 distanceVector = target.transform.position + (new Vector3(targetOffset.x, targetOffset.y, 0)) - transform.position;
+            float distanceFromTarget = distanceVector.magnitude;
+            transform.Translate(new Vector3(distanceVector.x, distanceVector.y, 0) * distanceFromTarget * followSpeed * Time.fixedDeltaTime);
+        }
+
+    }
+
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.width), fadeTexture);
+    }
+
+    public IEnumerator fade(bool fadeIn, float fadeTime = 1.0f)
+    {
+        float multiplier = fadeIn ? -1.0f : 1.0f;
+        float start = fadeIn ? 1.0f : 0.0f;
+        float a = 0;
+        for (float i = 0; i < fadeTime; i += Time.deltaTime)
+        {
+            a = (i / fadeTime) * multiplier;
+            fadeTexture.SetPixel(0, 0, new Color(fadeColor.r, fadeColor.g, fadeColor.b, start + a));
+            fadeTexture.Apply();
+            yield return null;
+        }
+        fadeTexture.SetPixel(0, 0, new Color(fadeColor.r, fadeColor.g, fadeColor.b, start + multiplier));
+        fadeTexture.Apply();
+    }
+}
---
+++ ShiverClock/Assets/Scripts/rotatePlatform.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotatePlatform : MonoBehaviour {
+    public enum RotationType
+    {
+        FULL_ROT,
+        HALF_ROT
+    }
+    public float rotationTime;
+    public RotationType rotationType;
+    private float sinVal = 0.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 newRot = transform.rotation.eulerAngles;
+
+        switch (rotationType)
+        {
+            case RotationType.FULL_ROT:
+                if (!Mathf.Approximately(rotationTime, 0))
+                {
+                    newRot.z += (Time.deltaTime * 360.0f) / (rotationTime);
+                }
+                break;
+            case RotationType.HALF_ROT:
+                if (!Mathf.Approximately( rotationTime,0))
+                {
+                    sinVal += Time.deltaTime/ (rotationTime * 9); //not really sure about this
+                    newRot.z = Mathf.Sin(sinVal * Mathf.Rad2Deg) * 90;
+                }
+                break;
+            default:
+                print("no rotType. default to FULL_ROT");
+                newRot.z += rotationTime * Time.deltaTime;
+                break;
+        }
+        transform.rotation = Quaternion.Euler(newRot);
+	}
+}
