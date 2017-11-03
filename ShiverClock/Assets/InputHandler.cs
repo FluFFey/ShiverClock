9f37b79ddbf928f0b3519eb49f29e0bd6d0af3c9
Henning Luick <henningel@gmail.com>
2017-11-03 20:14:37 +0100

    added first simple arena with materials. Added player with simple input and material and some folder structure

---
+++ ShiverClock/Assets/InputHandler.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using UnityEngine;
+
+enum HERO_STATE
+{
+    IDLE,
+    MOVING,
+    DASHING,
+    NO_OF_STATES
+}
+
+enum GHOST_STATE
+{
+    HUMAN,
+    GHOST
+}
+
+enum MOVE_DIRECTION
+{
+    UP = 1,
+    DOWN = 2,
+    LEFT = 4,
+    RIGHT = 8,
+    UPLEFT = UP + LEFT,
+    UPRIGHT = UP + RIGHT,
+    DOWNLEFT = DOWN + LEFT,
+    DOWNRIGHT = DOWN + RIGHT
+}
+
+struct Inputs
+{
+    public bool up;
+    public bool down;
+    public bool left;
+    public bool right;
+    public bool space;
+}
+
+public class InputHandler : MonoBehaviour
+{
+
+    public int moveSpeed = 1;
+    private void Awake()
+    {
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        float speed = 2.0f;
+        Vector2 newVelocity = new Vector3(0, 0);
+        //newVelocity.y = Input.GetAxis("Horizontal");
+        newVelocity.x = Input.GetAxis("Horizontal");
+        newVelocity = newVelocity.normalized * speed;
+        gameObject.GetComponent<Rigidbody2D>().velocity = newVelocity;
+    }
+}
