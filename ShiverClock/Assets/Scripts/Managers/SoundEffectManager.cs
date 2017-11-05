056d8662e86ec16c2572eedad67d55312e9de102
Oskar G <oskaragarshol@gmail.com>
2017-11-05 10:53:43 +0100

    Actually staged the relevant files before committing.

---
+++ ShiverClock/Assets/Scripts/Managers/SoundEffectManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffectManager : MonoBehaviour
+{
+    public static SoundEffectManager instance;
+    private List<AudioSource> _audioSources;
+    private float _timeScale = 1.0f;
+    private float _previousTimeScale = 1.0f;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            DestroyObject(gameObject);
+        }
+        DontDestroyOnLoad(this);
+    }
+
+    public void 
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
